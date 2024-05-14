4ee310fc2a6a2d00694c0744cb0b949a750ce8a6
Leilane Henrique <leilanehenrique18@gmail.com>
2024-05-14 15:06:05 -0300

    add read and delete file

---
+++ 6-Node.js/class-08-uploads/src/controllers/fileControllers.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import { UPLOADS_FOLDER } from "../configs/uploadConfigs";
+import path from "path";
+import fs from "fs";
+
+export const fileControllers = {
+  upload(req: Request, res: Response, next: NextFunction) {
+    try {
+      if (!req.file) return;
+
+      const { filename, size } = req.file;
+
+      const myFile = {
+        name: filename,
+        path: `localhost:3000/file/${filename}`,
+        size: Number(size / 1024).toFixed(1) + "KB",
+      };
+
+      return res.status(200).json({ message: "file!", myFile });
+    } catch (error) {
+      return next(error);
+    }
+  },
+
+  async delete(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { name } = req.params;
+      const filePath = path.resolve(UPLOADS_FOLDER, name);
+
+      await fs.promises.stat(filePath);
+      await fs.promises.unlink(filePath);
+
+      return res.status(200).json({ message: "delete!" });
+    } catch (error) {
+      return next(error);
+    }
+  },
+};
